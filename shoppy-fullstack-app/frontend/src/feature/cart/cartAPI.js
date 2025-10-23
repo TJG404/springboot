f4326d442960b06b06eef43023266055a80c8890
lee <lkm1401@gmail.com>
2025-10-23 17:16:37 +0900

    2025-10-23 commit

--- shoppy-fullstack-app/frontend/src/feature/cart/cartAPI.js
+++ shoppy-fullstack-app/frontend/src/feature/cart/cartAPI.js
@@ -4,36 +4,26 @@ import { addCartItem, updateCartCount,
          updateCartItem, removeCartItem } from './cartSlice.js';
 import { axiosData, axiosPost } from '../../utils/dataFetch.js';
 
-
-/** 회원아이디별 장바구니 카운트 */
 export const getCartCount = (id) => async(dispatch) => {
     const url = "/cart/count";
     const data = {"id": id};
     const jsonData = await axiosPost(url, data);
     dispatch(updateCartCount({"count": jsonData.sumQty}));
-//    return jsonData.sumQty;
 }
 
 export const removeCart = (cid) => async(dispatch) => {
     const url = "/cart/deleteItem";
     const data = {"cid": cid};
     const rows = await axiosPost(url, data);
-    console.log("delete rows => ", rows);
     const { userId } = JSON.parse(localStorage.getItem("loginInfo"));
     dispatch(getCartCount(userId));
     dispatch(showCart());
-
-//    dispatch(removeCartItem({"cid": cid}));
-//    dispatch(updateTotalPrice());
-//    dispatch(updateCartCount());
 }
 
 export const showCart = () => async (dispatch) => {
-//    const jsonData = await axiosData("/data/products.json");
     const url = "/cart/list";
     const { userId } = JSON.parse(localStorage.getItem("loginInfo"));
     const jsonData = await axiosPost(url, {"id": userId});
-    console.log("jsonData showcart-->", jsonData);
     dispatch(showCartItem({"items": jsonData}));
     dispatch(updateTotalPrice({"totalPrice" : jsonData[0].totalPrice}));
 }
@@ -42,23 +32,16 @@ export const updateCart = (cid, type) => async(dispatch) => {
     const url = "/cart/updateQty";
     const data = {"cid": cid, "type": type};
     const rows = await axiosPost(url, data);
-    console.log('updateCart rows => ', rows, type);
     const { userId } = JSON.parse(localStorage.getItem("loginInfo"));
     dispatch(getCartCount(userId));
     dispatch(showCart());
     return rows;
-//    dispatch(updateCartItem({"cid":cid, "type":type})); //수량변경
-//    dispatch(updateTotalPrice());
-//    dispatch(updateCartCount());
 }
 
 export const checkQty = async(pid, size, id) => {
-    //쇼핑백 추가한 상품과 사이즈가 DB 테이블에 있는지 유무 확인
     const url = "/cart/checkQty";
     const data = {"pid": pid, "size": size, "id": id};
-console.log(data);
     const jsonData = await axiosPost(url, data);
-console.log("jsonData --> ", jsonData);
     return jsonData;
 }
 
@@ -75,7 +58,5 @@ export const addCart = (pid, size) => async (dispatch) => {
         alert("상품이 추가되었습니다");
       }
       dispatch(getCartCount(userId));
-
-    return 1;
 }
 
